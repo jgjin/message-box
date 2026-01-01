4a9fedb9651f6e3c6db6e2d049c283be844d026f
Jason Jin <jgjin@umich.edu>
2026-01-01 18:58:33 -0500

    responds with plain text responses

--- server/main.py
+++ server/main.py
@@ -1,15 +1,17 @@
 from fastapi import FastAPI, Body
+from fastapi.responses import PlainTextResponse
+
 
 app = FastAPI()
 messages: dict[str, str] = {}
 
 
-@app.get("/message/{key}")
+@app.get("/message/{key}", response_class=PlainTextResponse)
 async def get_message(key: str):
     return messages.get(key, "")
 
 
-@app.post("/message/{key}")
+@app.post("/message/{key}", response_class=PlainTextResponse)
 async def post_message(key: str, message: str = Body()):
     messages[key] = message
 
